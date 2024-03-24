71c55b13fd92f67ac954da6b8e80adceeb915de3
Nikolaos Psaltakis <nickpsal@gmail.com>
2024-03-24 18:39:50 +0200

    commit

--- application/views/Dashboard/home.php
+++ application/views/Dashboard/home.php
@@ -1,7 +1,31 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
+<div class="b-example-divider b-example-vr"></div>
 
-<div class="container">
-	Welcome <?=$_SESSION['u_name']?>
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 250px;">
+	<ul class="nav nav-pills flex-column mb-auto">
+		<span class="fs-4">Employee Actions</span>
+		<li class="nav-item">
+			<a href="#" class="nav-link link-body-emphasis">
+				Add Action
+			</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link link-body-emphasis">
+				Actions List
+			</a>
+		</li>
+		<span class="fs-4">Jobs Actions</span>
+		<li>
+			<a href="#" class="nav-link link-body-emphasis">
+				Add Job
+			</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link link-body-emphasis">
+				Jobs List
+			</a>
+		</li>
+	</ul>
 </div>
