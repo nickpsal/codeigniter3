74c38e77a2bbd6f5836c643cdbdc6e1909967789
Nikolaos Psaltakis <nickpsal@gmail.com>
2024-03-24 19:17:37 +0200

    commit

--- application/views/Dashboard/home.php
+++ application/views/Dashboard/home.php
@@ -1,29 +1,26 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
-<div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 250px;">
-    <ul class="nav nav-pills flex-column mb-auto">
-        <span class="fs-4">Employee Actions</span>
-        <li class="nav-item">
-            <a href="#" class="nav-link link-body-emphasis">
-                Add Action
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link link-body-emphasis">
-                Actions List
-            </a>
-        </li>
-        <span class="fs-4">Jobs Actions</span>
-        <li class="nav-item">
-            <a href="#" class="nav-link link-body-emphasis">
-                Add Job
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link link-body-emphasis">
-                Jobs List
-            </a>
-        </li>
-    </ul>
+<div class="d-flex flex-row">
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 250px;">
+        <ul class="nav nav-pills flex-column mb-auto">
+            <span class="fs-4">Employee Actions</span>
+            <li class="nav-item">
+                <a href="#" class="nav-link link-body-emphasis">Add Action</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link link-body-emphasis">Actions List</a>
+            </li>
+            <span class="fs-4">Jobs Actions</span>
+            <li class="nav-item">
+                <a href="#" class="nav-link link-body-emphasis">Add Job</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link link-body-emphasis">Jobs List</a>
+            </li>
+        </ul>
+    </div>
+    <div class="container flex-grow-1">
+        Hello
+    </div>
 </div>
