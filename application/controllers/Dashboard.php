2848697b0fb16069ce9088a6d8d10de7f5a3b55a
Nikolaos Psaltakis <nickpsal@gmail.com>
2024-03-24 18:06:50 +0200

    commit

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,9 +5,19 @@
         public function index()
 		{
 			if (isset($_SESSION['u_name'])) {
-                $this->load->view('includes/header');
+                $this->load->view('dashboard/includes/header');
+                $this->load->view('dashboard/includes/nav');
                 $this->load->view('dashboard/home');
-                $this->load->view('includes/footer');
+                $this->load->view('dashboard/includes/footer');
+            }else {
+                redirect('home', 'refresh');
+            }
+		}
+
+        public function logout() {
+			if (isset($_SESSION['u_name'])) {
+                unset($_SESSION['u_name']);
+                redirect('home', 'refresh');
             }else {
                 redirect('home', 'refresh');
             }
